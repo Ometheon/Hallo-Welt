6406bffd64e3e6c1b4a0f08d35982c57b5a48b10
Ometheon <samuel_mueller1@gmx.de>
2026-09-17 11:52:22 +0200

    Änderung an test.py

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ print("Dies ist ein Test.")
 name = input("Wie ist dein Name?")
 
 print(f"Dein Name ist: {name}.")
+
+if name == "Miraak":
+    print("Ihr seid Miraak, das erste Drachenblut.")
+else:
+    print("Ihr seid nicht Miraak. Ich bin Miraak.")
